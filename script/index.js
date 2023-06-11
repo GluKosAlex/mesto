b7bf6e9a3fe03d62c763f581ecb3b731760e6986
GluKosAlex <glukosalex@gmail.com>
2023-06-11 16:20:24 +0700

    fix(index): hotfix after review 1

---
+++ script/index.js
@@ -0,0 +1,41 @@
+// Modal window search
+const modal = document.querySelector('.modal');
+const editBtn = document.querySelector('.profile__edit');
+const closeBtn = modal.querySelector('.modal__close');
+// Form search
+const form = modal.querySelector('.form');
+const formUsername = form.querySelector('.form__text-input_data_username');
+const formAbout = form.querySelector('.form__text-input_data_about');
+const formSubmit = form.querySelector('.form__button');
+// Profile search
+const profile = document.querySelector('.profile');
+const profileUserName = profile.querySelector('.profile__username');
+const profileAbout = profile.querySelector('.profile__about');
+
+// Modal open handler
+function openModal() {
+  formUsername.value = profileUserName.textContent; // Copy username from info to form username text field
+  formAbout.value = profileAbout.textContent; // Copy 'about' text from info to form 'about' text field
+  modal.classList.add('modal_opened');
+  formUsername.focus(); // Focus on username field after modal open
+};
+
+// Modal close handler
+function closeModal(evt) {
+  if (evt.target === evt.currentTarget) { // Prevent closing window by click on the child nodes of the 'modal'
+    modal.classList.remove('modal_opened');
+  }
+};
+
+// Modal open/close handle
+editBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+modal.addEventListener('click', closeModal);
+
+// Profile info change handle
+form.addEventListener('submit', evt => {
+  evt.preventDefault(); // Prevent form sending
+  profileUserName.textContent = formUsername.value;
+  profileAbout.textContent = formAbout.value;
+  closeModal(evt); // Close modal after save
+});
