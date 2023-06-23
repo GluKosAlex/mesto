c6af93b92b8f2748a97dec371b883be964dc5d94
GluKosAlex <glukosalex@gmail.com>
2023-06-23 13:01:55 +0700

    feat(index): add the rest functionality

--- script/index.js
+++ script/index.js
@@ -25,23 +25,45 @@ const initialCards = [
   }
 ];
 
-// Modal window search
-const modal = document.querySelector('.modal');
-const editBtn = document.querySelector('.profile__edit');
-const closeBtn = modal.querySelector('.modal__close');
-
-// Form search
-const form = modal.querySelector('.form');
-const formUsername = form.querySelector('.form__text-input_data_username');
-const formAbout = form.querySelector('.form__text-input_data_about');
 // Profile search
-const profile = document.querySelector('.profile');
-const profileUserName = profile.querySelector('.profile__username');
-const profileAbout = profile.querySelector('.profile__about');
+const profileInfo = document.querySelector('.profile');
+const profileUserName = profileInfo.querySelector('.profile__username');
+const profileAbout = profileInfo.querySelector('.profile__about');
+
+// Modal windows search
+const modals = document.querySelectorAll('.modal');
+const modalProfileEdit = document.querySelector('.modal_type_profile-edit');
+const modalCardAdd = document.querySelector('.modal_type_card-add');
+const modalImage = document.querySelector('.modal_type_image');
+
+// Modals open buttons search
+const btnProfileEdit = profileInfo.querySelector('.profile__edit');
+const btnCardAdd = profileInfo.querySelector('.profile__add-btn');
+
+// Image info search
+const image = modalImage.querySelector('.modal__image');
+const imageTitle = modalImage.querySelector('.modal__image-title');
+
+// Profile edit form search
+const formProfileEdit = modalProfileEdit.querySelector('.form');
+const formProfileEditUsername = formProfileEdit.querySelector('.form__text-input_data_username');
+const formProfileEditAbout = formProfileEdit.querySelector('.form__text-input_data_about');
+
+// Card add form search
+const formCardAdd = modalCardAdd.querySelector('.form');
+const formCardAddImgTitle = formCardAdd.querySelector('.form__text-input_data_img-title');
+const formCardAddImgUrl = formCardAdd.querySelector('.form__text-input_data_img-url');
+
 // Template search
 const cardTemplate = document.querySelector('#element-template').content;
+
 // Cards list search
 const cards = document.querySelector('.elements');
+
+// Add click event to target node
+function addClickEventTo(target, handler) {
+  target.addEventListener('click', handler);
+}
 
 // Card render handler
 function renderCard({name, link}) {
@@ -58,54 +80,101 @@ function renderCard({name, link}) {
   addClickEventTo(cardLikeButton, () => {
     cardLikeButton.classList.toggle('element__like_active')
   });
+
   addClickEventTo(cardDeleteButton, () => {
     const parentCard = cardDeleteButton.closest('.element');
     parentCard.remove();
   });
+
   addClickEventTo(cardImage, () => {
-    const parentCard = cardDeleteButton.closest('.element');
-    parentCard.remove();
+    openImageModal();
+    image.src = cardImage.src;
+    imageTitle.textContent = cardTitle.textContent;
   });
 
   cards.prepend(card);
 };
-
-// Add click event to target node
-function addClickEventTo(target, handler) {
-  target.addEventListener('click', handler);
-}
 
 // Cards from array render handler
 function renderCardsFromArray(arr) {
   arr.forEach(renderCard);
 }
 
-// Modal open handler
-function openModal() {
-  formUsername.value = profileUserName.textContent; // Copy username from info to form username text field
-  formAbout.value = profileAbout.textContent; // Copy 'about' text from info to form 'about' text field
-  modal.classList.add('modal_opened');
-  formUsername.focus(); // Focus on username field after modal open
+// Modal Profile edit open handler
+function openProfileEditModal() {
+  formProfileEditUsername.value = profileUserName.textContent;
+  formProfileEditAbout.value = profileAbout.textContent;
+  modalProfileEdit.classList.add('modal_opened');
+  setTimeout(() => {
+    formProfileEditUsername.focus(); // Focus on username field after modal open
+  }, 100); // Set timeout to prevent problem with visibility animation
+};
+
+// Modal Card add open handler
+function openCardAddModal() {
+  formCardAddImgTitle.value = '';
+  formCardAddImgUrl.value = '';
+  modalCardAdd.classList.add('modal_opened');
+  setTimeout(() => {
+    formCardAddImgTitle.focus(); // Focus on image title field after modal open
+  }, 100); // Set timeout to prevent problem with visibility animation
+};
+
+// Modal Image open handler
+function openImageModal() {
+  modalImage.classList.add('modal_opened');
 };
 
 // Modal close handler
-function closeModal() {
-  modal.classList.remove('modal_opened');
+function closeModal(evt) {
+  evt.target.closest('.modal').classList.remove('modal_opened');
 };
 
-function formSubmitHandler(evt) {
+// Add close handler to close buttons
+function addCloseHandlerTo(items) {
+  for (let item of items) {
+    const closeBtn = item.querySelector('.modal__close');
+    addClickEventTo(closeBtn, closeModal);
+  }
+}
+
+// Save info from profile edit form inputs to profile content handler
+function saveProfileInfo(evt) {
   evt.preventDefault(); // Prevent form sending
-  profileUserName.textContent = formUsername.value;
-  profileAbout.textContent = formAbout.value;
-  closeModal(); // Close modal after save
+
+  profileUserName.textContent = formProfileEditUsername.value;
+  profileAbout.textContent = formProfileEditAbout.value;
+
+  closeModal(evt); // Close modal after save
 };
 
-// Modal open/close handle
-addClickEventTo(editBtn, openModal);
-addClickEventTo(closeBtn, closeModal);
+// Save info from card add form inputs handler
+function saveCardInfo(evt) {
+  evt.preventDefault(); // Prevent form sending
+  const newCardInfo = {};
 
-// Profile info change handle
-form.addEventListener('submit', formSubmitHandler);
+  newCardInfo.name = formCardAddImgTitle.value;
+  newCardInfo.link = formCardAddImgUrl.value;
+
+  renderCard(newCardInfo);
+
+  closeModal(evt); // Close modal after save
+};
 
 // Render Initial cards
 renderCardsFromArray(initialCards);
+
+// Profile info change handle
+formProfileEdit.addEventListener('submit', saveProfileInfo);
+
+// Card add handle
+formCardAdd.addEventListener('submit', saveCardInfo);
+
+// Add close handler to close buttons of modal windows
+addCloseHandlerTo(modals);
+
+// Modal Profile edit open handle
+addClickEventTo(btnProfileEdit, openProfileEditModal);
+
+// Modal Card add open handle
+addClickEventTo(btnCardAdd, openCardAddModal);
